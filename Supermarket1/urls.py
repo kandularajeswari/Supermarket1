73dcd302f6e111144640e90a630d7b43d9198b82
kandula rajeswari <kandularaji1230@gmail.com>
2021-09-25 10:34:34 +0530

    first release

--- Supermarket1/urls.py
+++ Supermarket1/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('supermarketapp/', include('supermarketapp.urls'))
+    path('supermarketapp/', include('supermarketapp.urls')),
+    
 ]
